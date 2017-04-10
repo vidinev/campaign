fe2330264d0946cb264387c32420b4d9ec36de83
Anton <vidinev777@gmail.com>
2017-04-10 20:01:08 +0300

    feat(initial) add sub header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SubHeaderComponent } from './shared/sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SubHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/sub-header/sub-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-header',
+  templateUrl: './sub-header.component.html',
+  styleUrls: ['./sub-header.component.scss']
+})
+export class SubHeaderComponent {
+
+  constructor() { }
+
+}
