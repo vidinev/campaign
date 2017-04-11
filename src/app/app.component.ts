dc0179cb71a8a06537ccd89725a7eda1f2a831da
Anton <vidinev777@gmail.com>
2017-04-11 14:34:32 +0300

    feat(initial) add form layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+/** TODO
+ * 1) Move global styles from components
+ * 2) Add redux to project
+ * 3) Handle sub header state via redux
+ * 4) Refactor components
+* */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
