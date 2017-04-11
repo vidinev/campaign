a4cb5ff409560c9157ffdf7a15bb0a9be733ebfb
Anton <vidinev777@gmail.com>
2017-04-11 19:54:54 +0300

    feat(initial) burger menu toggle

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,18 @@ export class HeaderComponent {
 
   public avatar: string = 'assets/images/empty-profile.jpg';
   public avatarImage: string = `url(${this.avatar})`;
+  public isOpen: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((evt: NavigationStart) => {
+      if (evt instanceof NavigationStart) {
+        this.isOpen = false;
+      }
+    });
+  }
+
+  toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 
 }
