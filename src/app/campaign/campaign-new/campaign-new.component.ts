edc0630c47912442228081183a9f2f24269af10a
Anton <vidinev777@gmail.com>
2017-04-11 17:21:52 +0300

    feat(initial) add dynamic validation

--- src/app/campaign/campaign-new/campaign-new.component.ts
+++ src/app/campaign/campaign-new/campaign-new.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
 
 const formBuilder = new FormBuilder();
+
+const pageConfiguration = {
+  name: ['', [
+    Validators.required,
+    Validators.minLength(1),
+    Validators.maxLength(30)
+  ]],
+  page: ['', [
+    Validators.required,
+    CustomValidators.url
+  ]],
+  percent: ['', [
+    Validators.required,
+    Validators.pattern(/^-?\d+$/),
+    CustomValidators.min(0),
+    CustomValidators.max(100)
+  ]]
+};
 
 const formConfiguration = {
   final: ['', [
@@ -20,9 +38,26 @@ const formConfiguration = {
 export class CampaignNewComponent {
 
   form: FormGroup;
+  percentMessages: { [name: string]: string; } = {
+    pattern: 'Please enter valid percent value',
+    max: 'Max value 100',
+    min: 'Min value 0'
+  };
 
   constructor() {
     this.form = formBuilder.group(formConfiguration);
+    this.addPage();
+  }
+
+  addPage() {
+    const pagesControl = <FormArray> this.form.controls['pages'];
+    const group = formBuilder.group(pageConfiguration);
+    pagesControl.push(group);
+  }
+
+  removePage(index: number) {
+    const pagesControl = <FormArray> this.form.controls['pages'];
+    pagesControl.removeAt(index);
   }
 
 }
